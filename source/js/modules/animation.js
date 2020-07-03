ee2ad7e849ba5927d815479fd7e1d94af7af9b27
Slava Slivka <aphelion.unet@gmail.com>
2020-07-04 00:05:00 +0300

    module2-task2

--- source/js/modules/animation.js
+++ source/js/modules/animation.js
@@ -21,4 +21,61 @@ class AnimateOverlay {
   }
 }
 
-export {animation, AnimateOverlay};
+class AccentTypographyBuild {
+  constructor(element, duration, property) {
+    this._element = document.querySelector(element);
+    this._duration = duration;
+    this._property = property;
+
+    this._divideWords();
+  }
+
+  _splitAtChar(word, wordIndex) {
+    return word.split(``).reduce((fragment, letter, letterIndex) => {
+      const container = document.createElement(`span`);
+      container.classList.add(`letter`);
+      container.textContent = letter;
+      container.style.transition = `${this._property} ${this._duration}ms ease ${this._getDelayForTransition(letterIndex, wordIndex)}ms`;
+      fragment.appendChild(container);
+
+      return fragment;
+    }, document.createDocumentFragment());
+  }
+
+  _divideWords() {
+    if (!this._element) {
+      return;
+    }
+
+    const text = this._element.textContent.trim().split(` `).reduce((fragment, word, i) => {
+      const container = document.createElement(`span`);
+
+      container.classList.add(`word`);
+      container.appendChild(this._splitAtChar(word, i));
+      fragment.appendChild(container);
+
+      return fragment;
+    }, document.createDocumentFragment());
+
+    this._element.innerHTML = ``;
+    this._element.appendChild(text);
+  }
+
+  _getDelayForTransition(letterIndex, wordIndex) {
+    return letterIndex % 2 === 0 ? (wordIndex * 400) + 200 : (wordIndex * 400) + 400;
+  }
+
+  init() {
+    if (!this._element) {
+      return;
+    }
+
+    this._element.classList.add(`active`);
+  }
+
+  destroy() {
+    this._element.classList.remove(`active`);
+  }
+}
+
+export {animation, AnimateOverlay, AccentTypographyBuild};
